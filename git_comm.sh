fd779188636b688f0abe80d54db7bed6470f33d0
MJ Dederi <mjdederi01@gmail.com>
2023-05-21 08:58:34 +0100

    git_comm.sh

--- git_comm.sh
+++ git_comm.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 files_list=$(ls)
+files=0
 for file in $files_list:
 do
+	files=$((files + 1))
 	git add "$file"
 	git commit -m "$file"
 	git push
 done
 
-echo "EVERYTHING pushed"
-git status
+echo "$files PUSHED!"
